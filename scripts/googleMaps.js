f88feebe0583a16c9324d244c9afbad07d3f007e
Sasha Shut <sanyashut159@gmail.com>
2023-03-02 21:07:49 +0200

    added 2 pages, but in future will fix different bugs

--- scripts/googleMaps.js
+++ scripts/googleMaps.js
@@ -1,29 +1,56 @@
-/* import { config } from './dotenv';
-config();
+// const initMap = () => {
+// 	// create a map object centered on a specific location
+// 	var map = new google.maps.Map(document.getElementById('google-map-main-page'), {
+// 		center: { lat: 37.7749, lng: -122.4194 },
+// 		zoom: 12
+// 	});
 
-const apiKey = process.env.API_KEY;
-console.log(apiKey);
 
-const script = document.createElement('script');
-script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
-script.async = true;
-script.defer = true;
 
-document.head.appendChild(script); */
+// 	// add a marker to the map
+// 	const marker = new google.maps.Marker({
+// 		position: { lat: 37.7749, lng: -122.4194 },
+// 		map: map,
+// 		title: 'San Francisco'
+// 	});
+// }
 
-const initMap = () => {
-	// create a map object centered on a specific location
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center: { lat: 37.7749, lng: -122.4194 },
-		zoom: 12
-	});
+// google.maps.event.addDomListener(window, 'load', initMap);
 
-	// add a marker to the map
-	const marker = new google.maps.Marker({
-		position: { lat: 37.7749, lng: -122.4194 },
-		map: map,
-		title: 'San Francisco'
-	});
-}
 
-google.maps.event.addDomListener(window, 'load', initMap);
+
+
+// ! slider test
+// const slider = document.querySelector('.slider');
+// const slides = document.querySelectorAll('.slide');
+// const prevBtn = document.querySelector('.prev');
+// const nextBtn = document.querySelector('.next');
+
+// let slideIndex = 0;
+
+// function showSlide () {
+// 	slides.forEach(slide => {
+// 		slide.style.transform = `translateX(-${slideIndex * 100}%)`;
+// 	});
+// }
+
+// function nextSlide () {
+// 	if (slideIndex >= slides.length - 1) {
+// 		slideIndex = 0;
+// 	} else {
+// 		slideIndex++;
+// 	}
+// 	showSlide();
+// }
+
+// function prevSlide () {
+// 	if (slideIndex <= 0) {
+// 		slideIndex = slides.length - 1;
+// 	} else {
+// 		slideIndex--;
+// 	}
+// 	showSlide();
+// }
+
+// nextBtn.addEventListener('click', nextSlide);
+// prevBtn.addEventListener('click', prevSlide);
---
+++ scripts/googleMapsTheAreaPage.js
@@ -0,0 +1,18 @@
+const initMap = () => {
+	// create a map object centered on a specific location
+	var map = new google.maps.Map(document.getElementById('map-the-area'), {
+		center: { lat: 37.7749, lng: -122.4194 },
+		zoom: 12
+	});
+
+
+
+	// add a marker to the map
+	const marker = new google.maps.Marker({
+		position: { lat: 37.7749, lng: -122.4194 },
+		map: map,
+		title: 'San Francisco'
+	});
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
