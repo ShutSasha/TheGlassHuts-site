7968f2f7bd9316aec7c0b1a269d8f7662e6bd668
Sasha Shut <sanyashut159@gmail.com>
2023-03-03 16:28:44 +0200

    fix google map

--- scripts/googleMaps.js
+++ scripts/googleMaps.js
@@ -1,21 +1,21 @@
-// const initMap = () => {
-// 	// create a map object centered on a specific location
-// 	var map = new google.maps.Map(document.getElementById('google-map-main-page'), {
-// 		center: { lat: 37.7749, lng: -122.4194 },
-// 		zoom: 12
-// 	});
+const initMap = () => {
+	// create a map object centered on a specific location
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: { lat: 37.7749, lng: -122.4194 },
+		zoom: 12
+	});
 
 
 
-// 	// add a marker to the map
-// 	const marker = new google.maps.Marker({
-// 		position: { lat: 37.7749, lng: -122.4194 },
-// 		map: map,
-// 		title: 'San Francisco'
-// 	});
-// }
+	// add a marker to the map
+	const marker = new google.maps.Marker({
+		position: { lat: 37.7749, lng: -122.4194 },
+		map: map,
+		title: 'San Francisco'
+	});
+}
 
-// google.maps.event.addDomListener(window, 'load', initMap);
+google.maps.event.addDomListener(window, 'load', initMap);
 
 
 
