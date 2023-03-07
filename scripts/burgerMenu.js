0e97d622e497eb4e579e34ab57b3c0d412983ef0
Sasha Shut <sanyashut159@gmail.com>
2023-03-07 16:51:29 +0200

    fix burger on phone 0.3

--- scripts/burgerMenu.js
+++ scripts/burgerMenu.js
@@ -4,10 +4,10 @@ const HTML = document.querySelector('html');
 burger.addEventListener('click', function () {
 	this.classList.toggle('active');
 	if (burger.classList.contains('active')) {
-		HTML.style.overflow = 'hidden !important';
+		HTML.style.overflowY = 'hidden';
 	}
 	else {
-		HTML.style.overflow = 'visible !important';
+		HTML.style.overflowY = 'visible';
 	}
 	document.querySelector('.nav-header').classList.toggle('open');
 })
