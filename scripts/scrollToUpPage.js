f0fd898a8be276d3ead98b7f6bd40ecebb9fe07e
Sasha Shut <sanyashut159@gmail.com>
2023-04-23 21:22:04 +0300

    first page in grid

--- scripts/scrollToUpPage.js
+++ scripts/scrollToUpPage.js
@@ -22,17 +22,3 @@ window.onscroll = () => {
 	}
 
 }
-
-// scroll to anchors
-const anchors = document.querySelectorAll('a[href*="#"]');
-
-for (let anchor of anchors) {
-	anchors.onclick = (event) => {
-		event.preventDefault();
-		const blockID = anchor.getAttribute('href');
-		document.querySelector('' + blockID).scrollIntoView({
-			behavior: "smooth",
-			block: "start"
-		})
-	}
-}
